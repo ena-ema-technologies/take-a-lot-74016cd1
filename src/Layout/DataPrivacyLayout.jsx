f6216e11c74e5b6ce22f18efe64a2e3e7984061f
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-11-06 00:16:45 +0600

    layot done

--- src/Layout/DataPrivacyLayout.jsx
+++ src/Layout/DataPrivacyLayout.jsx
@@ -6,20 +6,25 @@ const DataPrivacyLayout = () => {
 
     const location = useLocation();
 
-    const availablePayment = location.pathname.includes("available-payment-options");
-    const priceItem = location.pathname.includes("price-item-change-checkout");
-    const recievedConfirmation = location.pathname.includes("havent-received-confirmation")
-    const couponCode = location.pathname.includes("all-about-coupon-codes-discounts")
-    const giftVoucher = location.pathname.includes("all-about-gift-vouchers");
-    const payFlex = location.pathname.includes("all-about-payflex");
-    const card = location.pathname.includes("all-about-credit-debit-card");
-    const cod = location.pathname.includes("all-about-cash-on-delivery");
-    const eftOzow = location.pathname.includes("all-about-instant-eFT-ozow");
-    const eftWithPayFast = location.pathname.includes("all-about-instant-EFT-payFast");
-    const mobiCred = location.pathname.includes("all-about-mobicred");
-    const eBucks = location.pathname.includes("all-about-eBucks");
-    const masterPass = location.pathname.includes("all-about-masterpass");
-    const discoveryMiles = location.pathname.includes("a-ll-about-discovery-miles");
+    const registerAccount = location.pathname.includes("register-to-takealot");
+    const resettingPassword = location.pathname.includes("resetting-password");
+    const unSubscriber = location.pathname.includes("unsubscribe-email-newsletters")
+    const personalInfo = location.pathname.includes("about-personal-information")
+    const profileUpdate = location.pathname.includes("profile-update-information");
+    const managingPersonal = location.pathname.includes("managing-personal");
+    const deleteAccount = location.pathname.includes("delete-account");
+    const trackStatus = location.pathname.includes("track-status-deletion");
+    const aboutPOPIA = location.pathname.includes("about-POPIA");
+    const verifyAccount = location.pathname.includes("verify-identity");
+    const difference = location.pathname.includes("between-POPIA-PAIA");
+    const security = location.pathname.includes("security-compromise");
+    const authorise = location.pathname.includes("authorise-another-person");
+    const accessFamilyMember = location.pathname.includes("access-deceased-family");
+    const cookies = location.pathname.includes("about-cookies");
+    const allowChildren = location.pathname.includes("children-allowed");
+    const verifyEmail = location.pathname.includes("verify-email-address");
+    const verifyPhone = location.pathname.includes("verify-mobile-number");
+
 
     return (
         <section className='lg:px-4'>
@@ -29,7 +34,7 @@ const DataPrivacyLayout = () => {
                 <div className="hidden lg:block text-xs breadcrumbs text-white absolute top-20 left-10">
                     <ul>
                         <li><Link to="/help-centre">Help Centre</Link></li>
-                        <li><Link to="help-centre/data-privacy">Data & Privacy  
+                        <li><Link to="help-centre/data-privacy">Data & Privacy
                         </Link>
                         </li>
                         <li className='font-semibold'>
@@ -70,34 +75,34 @@ const DataPrivacyLayout = () => {
                         <div className='border-b border-[#dadada]'></div>
 
                         <div className='my-3 flex flex-col gap-2'>
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/available-payment-options" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-primary")}> Available payment options </NavLink>
+                            <NavLink to="/help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-primary")}> How to register on takealot.com </NavLink>
 
 
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/price-item-change-checkout" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> Why does the price of my item change when I try and check out ? </NavLink>
+                            <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> Why does the price of my item change when I try and check out ? </NavLink>
 
 
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/havent-received-confirmation" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> Why haven’t I received my payment confirmation email ?
+                            <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> Why haven’t I received my payment confirmation email ?
                             </NavLink>
 
 
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-coupon-codes-discounts" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about coupon codes & discounts
+                            <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about coupon codes & discounts
                             </NavLink>
 
 
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-gift-vouchers" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about gift vouchers </NavLink>
+                            <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about gift vouchers </NavLink>
 
 
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-payflex" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-transparent")}>  All about PayFlex </NavLink>
+                            <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-transparent")}>  All about PayFlex </NavLink>
 
 
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-credit-debit-card" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Credit / Debit Card Payments </NavLink>
+                            <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Credit / Debit Card Payments </NavLink>
 
 
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-cash-on-delivery" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Cash on Delivery ( COD ) </NavLink>
+                            <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Cash on Delivery ( COD ) </NavLink>
 
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-instant-eFT-ozow" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Instant EFT with Ozow </NavLink>
+                            <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Instant EFT with Ozow </NavLink>
 
-                            <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-instant-EFT-payFast" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Instant EFT with PayFast
+                            <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Instant EFT with PayFast
                             </NavLink>
 
                             {/* extra nav */}
@@ -105,16 +110,24 @@ const DataPrivacyLayout = () => {
                             {
                                 viewExpandState ?
                                     <>
-                                        <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-mobicred" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Mobicred
+                                        <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Mobicred
                                         </NavLink>
 
-                                        <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-eBucks" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about eBucks
+                                        <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about eBucks
                                         </NavLink>
 
-                                        <NavLink to="/help-centre/payments-promos-gift-vouchers-details/all-about-masterpass" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Masterpass
+                                        <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Masterpass
                                         </NavLink>
 
-                                        <NavLink to="/help-centre/payments-promos-gift-vouchers-details/a-ll-about-discovery-miles" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Discovery Miles
+                                        <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Discovery Miles
+                                        </NavLink>
+                                        <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Discovery Miles
+                                        </NavLink>
+                                        <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Discovery Miles
+                                        </NavLink>
+                                        <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Discovery Miles
+                                        </NavLink>
+                                        <NavLink to="help-centre/data-privacy-details/register-to-takealot" className={({ isActive }) => (isActive ? "bg-primary w-[90%] pl-3 py-2 rounded-r-full bg-opacity-10 border-l-4 border-primary text-primary" : "bg-transparent hover:bg-gray-100 w-[90%] pl-3 py-2  rounded-r-full bg-opacity-10 border-l-4 border-transparent")}> All about Discovery Miles
                                         </NavLink>
 
                                     </>
