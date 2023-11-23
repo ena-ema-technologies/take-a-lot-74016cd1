6f9186b5452d46965eb2d640408a9c60da77dcd5
Saiful Islam <sakibbhaiya143@gmail.com>
2023-11-23 12:00:31 +0600

    New 2 page added

--- src/Page/SellerAccount/ManageOffers/ManageOffers.jsx
+++ src/Page/SellerAccount/ManageOffers/ManageOffers.jsx
@@ -1,10 +1,260 @@
 import React from 'react';
+import DashboardHead from '../../../components/DashboardHead/DashboardHead';
+import { useState } from 'react';
+import { IoSearchOutline } from 'react-icons/io5';
+import { HiOutlineDownload } from "react-icons/hi";
+import { HiMiniArrowTopRightOnSquare } from 'react-icons/hi2';
 
 const ManageOffers = () => {
+    const [tabName, setTabName] = useState("Manage My Offers")
     return (
-        <div>
-            <h1>Manage Offers</h1>
-        </div>
+<section className='w-[99%] relative h-full'>
+    <nav className='absolute left-0 right-0 top-0'>
+        <DashboardHead title={tabName}/>
+    </nav>
+
+
+    <div className='flex overflow-auto lg:overflow-hidden w-full lg:w-[99%] mx-auto items-center justify-start text-sm text-neutral border-t border-l border-r mt-16'>
+
+<div onClick={()=>setTabName("Manage My Offers")} className={`px-10 py-3 cursor-pointer inline-flex items-center gap-1 ${tabName === "Manage My Offers" ? "bg-white border-t-2 border-t-primary text-primary" : ""}`}> Manage My Offers</div>
+
+<div onClick={()=>setTabName("View Bulk Result")} className={`px-10 py-3 cursor-pointer inline-flex items-center gap-1 ${tabName === "View Bulk Result" ? "bg-white border-t-2 border-t-primary text-primary" : ""}`}> View Bulk Result</div>
+</div>
+
+{
+    tabName === "Manage My Offers" 
+    ?
+    <div className='w-full lg:w-[99%] mx-auto h-full bg-white'>
+<div className='pt-6 w-full flex items-center justify-between'>
+
+<div className='w-full flex items-center'>
+<div>
+<select id="selectOption" name="selectOption" className='px-4 py-1 text-sm border rounded'>
+        <option value="option1">Search by Product Title</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+    </select>
+</div>
+
+<div className='w-full relative'>
+    <input type="text" placeholder='Search...' className='w-full border-t border-b border-r px-2 py-1 text-sm placeholder:font-medium placeholder:text-[#000] outline-none'/>
+    <IoSearchOutline className='absolute right-4 top-2'/>
+</div>
+</div>
+
+<div className='w-full flex justify-end items-center gap-3'>
+<div className='text-primary text-sm inline-flex items-center gap-1'>
+<HiOutlineDownload className='h-5 w-5'/> <span className='text-xs font-medium'>Download Template</span>
+</div>
+
+<div className='text-xs text-white font-medium px-2 py-1 bg-neutral rounded'>
+<HiMiniArrowTopRightOnSquare className='inline-flex items-center h-4 w-4'/>  Export My Offers
+</div>
+
+<div className='text-xs text-white font-medium px-2 py-1 bg-neutral rounded'>
+    Bulk Upload
+</div>
+</div>
+
+</div>
+
+
+<div className='pt-6 w-full flex items-center justify-between'>
+
+<div className='w-full flex items-center gap-4'>
+<div>
+<select id="selectOption" name="selectOption" className='px-4 py-1 text-sm border rounded'>
+        <option value="option1">Search by Product Title</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+    </select>
+</div>
+
+<div className='w-full relative'>
+<select id="selectOption" name="selectOption" className='px-4 py-1 text-sm border rounded w-1/2'>
+        <option value="option1">Filter</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+    </select>
+</div>
+
+<div>
+    //TODO
+</div>
+</div>
+
+<div className='w-full flex justify-end items-center gap-3'>
+<div className='text-primary text-sm inline-flex items-center gap-1'>
+<input type="checkbox" id="showProductImage" name="showProductImage" />
+<label for="showProductImage" className='text-[#000] text-xs'>Show Product Image</label>
+
+</div>
+
+</div>
+
+</div>
+
+<div className='border mt-6 py-3 px-2 w-full'>
+
+<div className='w-full items-center justify-between'>
+<div className='w-full inline-flex items-center gap-2 text-xs font-medium'>
+<label>Show</label>
+<select id="selectOption" name="selectOption" className='px-2 py-1 text-sm border rounded'>
+        <option value="10">10</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+    </select>
+    <span>per page</span>
+</div>
+</div>
+
+<div className="overflow-x-auto mt-5 w-[99%] mx-auto">
+  <table className="table text-xs">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox checkbox-xs" />
+          </label>
+        </th>
+        <th>Image</th>
+        <th>Product Details</th>
+        <th>Pricing Details</th>
+        <th>Leadtime Stock</th>
+        <th>Stock At Takealot</th>
+        <th>Stock Days Cover</th>
+        <th>Stock On Way</th>
+        <th>Last 30 Days</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox checkbox-xs" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Null
+        </td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+      </tr>
+    </tbody>
+    
+  </table>
+</div>
+</div>
+</div> : 
+<div className='w-full lg:w-[99%] mx-auto h-full bg-white'>
+<div className='pt-6 px-3 flex items-center justify-start'>
+
+
+<div className='flex justify-start items-center gap-3'>
+<div className='text-primary text-sm inline-flex items-center gap-1'>
+<HiOutlineDownload className='h-5 w-5'/> <span className='text-xs font-medium'>Download Template</span>
+</div>
+
+
+<div className='text-xs text-white font-medium px-2 py-1 bg-neutral rounded'>
+    Bulk Upload
+</div>
+</div>
+
+</div>
+
+
+
+<div className='border mt-6 py-3 px-2 w-[90%]'>
+
+<div className='w-full items-center justify-between'>
+<div className='w-full inline-flex items-center gap-2 text-xs font-medium'>
+<label>Show</label>
+<select id="selectOption" name="selectOption" className='px-2 py-1 text-sm border rounded'>
+        <option value="10">10</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+    </select>
+    <span>per page</span>
+</div>
+</div>
+
+<div className="overflow-x-auto mt-5 w-[99%] mx-auto">
+  <table className="table text-xs">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox checkbox-xs" />
+          </label>
+        </th>
+        <th>Date Submitted</th>
+        <th>Status</th>
+        <th>File Name</th>
+        <th>File ID</th>
+        <th>Updates Requested</th>
+        <th>Fully Updated</th>
+        <th>Failed To Fully Update</th>
+        <th>Created</th>
+        <th>Download Original</th>
+        <th>Download Results</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox checkbox-xs" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Null
+        </td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+        <td>Null</td>
+      </tr>
+    </tbody>
+    
+  </table>
+</div>
+</div>
+</div>
+}
+
+</section>
     );
 };
 
--- src/Page/SellerAccount/SellerDashboard/DashboardHome.jsx
+++ src/Page/SellerAccount/SellerDashboard/DashboardHome.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { FaBuilding, FaDownload, FaTasks } from 'react-icons/fa';
-import { HiMiniBell, HiMiniCog, HiMiniCog8Tooth, HiOutlineClipboardDocumentList } from 'react-icons/hi2';
+import { HiMiniBell, HiMiniCog, HiMiniCog8Tooth, HiOutlineChevronRight, HiOutlineClipboardDocumentList } from 'react-icons/hi2';
 import { IoIosCard, IoIosSend, IoMdBusiness } from 'react-icons/io';
 import { IoPeopleCircleOutline, IoPersonCircleOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
@@ -26,7 +26,9 @@ const DashboardHome = () => {
 </div>
     </div>
 
-    <div className='mt-7 flex items-center gap-5'>
+    <div className='mt-7 flex gap-5 flex-col lg:flex-row'>
+
+<div className='w-full'>
 <div className='w-full h-[85px] bg-black bg-opacity-80 flex items-center justify-between px-2'>
 <div> 
     <p className='text-white text-sm font-semibold'>Boost your with Promotions & Daily Deals</p>
@@ -37,6 +39,49 @@ const DashboardHome = () => {
 </div>
 </div>
 
+<div className='mt-3 w-full bg-white shadow px-2 py-3'>
+
+<p className='text-sm font-bold'>Leadtime Orders</p>
+<div className="border-t mt-1"></div>
+
+<div className='mt-2 space-y-2 w-full text-sm'>
+   <div className='w-full flex items-center justify-between'>
+    <p>New</p> 
+    <p className='inline-flex items-center gap-2'><span className='bg-yellow-500 px-2 text-xs font-semibold text-white rounded-l-full rounded-r-full'>1 URGENT</span> 41 <HiOutlineChevronRight className='h-4 w-4'/></p>
+    </div>
+
+    <div className='w-full flex items-center justify-between'>
+    <p>Draft Shipment</p> 
+    <p className='inline-flex items-center gap-2'> 0 <HiOutlineChevronRight className='h-4 w-4'/></p>
+    </div>
+
+    <div className='w-full flex items-center justify-between'>
+    <p>Confirmed Shipment</p> 
+    <p className='inline-flex items-center gap-2'> 58 <HiOutlineChevronRight className='h-4 w-4'/></p>
+    </div>
+
+    <div className='w-full flex items-center justify-between'>
+    <p>Shipped Shipment</p> 
+    <p className='inline-flex items-center gap-2'><span className='bg-yellow-500 px-2 text-xs font-semibold text-white rounded-l-full rounded-r-full'>10 URGENT</span> 51 <HiOutlineChevronRight className='h-4 w-4'/></p>
+    </div>
+</div>
+
+</div>
+
+<div className='mt-3 w-full bg-white shadow px-2 py-3'>
+
+<p className='text-sm font-bold'>Recent Communication</p>
+<div className="border-t mt-1"></div>
+
+<div className='mt-2 space-y-2 w-full text-sm'>
+<p>No communication content here!</p>
+</div>
+
+</div>
+</div>
+
+
+<div className='w-full'>
 <div className='w-full h-[85px] bg-black bg-opacity-80 flex items-center justify-between px-2'>
 <div> 
     <p className='text-white text-sm font-semibold'>Increase Sales with Sponsored Product Ads</p>
@@ -45,6 +90,44 @@ const DashboardHome = () => {
 
 <div>
     <button className='bg-primary text-sm px-4 py-[2px] rounded text-white'>Apply Now</button>
+</div>
+</div>
+
+<div className='mt-3 w-full bg-white shadow px-2 py-3'>
+
+<p className='text-sm font-bold'>Removal Orders</p>
+<div className="border-t mt-1"></div>
+
+<div className='mt-2 space-y-2 w-full text-sm'>
+
+    <div className='w-full flex items-center justify-between'>
+    <p>Submitted</p> 
+    <p className='inline-flex items-center gap-2'> 2 <HiOutlineChevronRight className='h-4 w-4'/></p>
+    </div>
+
+    <div className='w-full flex items-center justify-between'>
+    <p>Ready for Pickup</p> 
+    <p className='inline-flex items-center gap-2'> 8 <HiOutlineChevronRight className='h-4 w-4'/></p>
+    </div>
+
+    <div className='w-full flex items-center justify-between'>
+    <p>Closed</p> 
+    <p className='inline-flex items-center gap-2'> 120 <HiOutlineChevronRight className='h-4 w-4'/></p>
+    </div>
+</div>
+
+</div>
+
+
+<div className='mt-3 w-full bg-white shadow px-2 py-3'>
+
+<p className='text-sm font-bold'>Customer Invoice Request</p>
+<div className="border-t mt-1"></div>
+
+<div className='mt-2 space-y-2 w-full text-sm'>
+<p>No invoice content here!</p>
+</div>
+
 </div>
 </div>
     </div>
